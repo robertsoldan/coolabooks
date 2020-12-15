8a8b53ff1fad0a196ed163523207dd00785a310b
Robert <soldanrobertandrei@gmail.com>
2020-12-15 10:45:28 +0000

    allowed hosts

--- coolabooks/coolabooks/settings.py
+++ coolabooks/coolabooks/settings.py
@@ -10,10 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
-from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-#BASE_DIR = Path(__file__).resolve().parent.parent
+# BASE_DIR = Path(__file__).resolve().parent.parent
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
@@ -26,7 +26,7 @@ SECRET_KEY = 'o#tw@@ke6dxitva+@@n4(l2jkfa@!wk!d38+%g$cax@l!p^w9!'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["coolabooks.herokuapp.com", "localhost", "127.0.0.1"]
 
 
 # Application definition
@@ -139,3 +139,7 @@ STATICFILES_DIRS = [
    ]
 # temporarily changing the redirect so it redirects to the home page until user dashboard is created
 LOGIN_REDIRECT_URL = 'books:index'
+
+# Configure Django App for Heroku.
+
+django_heroku.settings(locals())
